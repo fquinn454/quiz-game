2afeb4f8c11cf1368e3513abb71a437186eb182b
Fiona <57137659+fquinn454@users.noreply.github.com>
2023-02-19 11:06:04 +0000

    javascript to change button colors

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const form = document.getElementById("q1");
+const answer_a = document.getElementById("q1-a");
+const answer_b = document.getElementById("q1-b");
+const answer_c = document.getElementById("q1-c");
+const answer_d = document.getElementById("q1-d");
 
+form.addEventListener("submit", function(event) {
+    const buttonClicked = event.submitter.name;
+    if (buttonClicked === 'q1-b'){
+        answer_b.style.backgroundColor = "green";
+    }
+    else{
+        answer_a.style.backgroundColor = "rgba(211, 28, 28, 1)";
+        answer_b.style.backgroundColor = "green";
+        answer_c.style.backgroundColor = "rgba(211, 28, 28, 1)";
+        answer_d.style.backgroundColor = "rgba(211, 28, 28, 1)";
+    }
+    event.preventDefault();
+  });
