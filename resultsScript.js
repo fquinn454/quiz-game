9a7f354dee50d23f05e17d9f2655dde57e4749b4
Fiona <57137659+fquinn454@users.noreply.github.com>
2023-02-26 17:31:45 +0000

    renamed file

--- resultsScript.js
+++ resultsScript.js
@@ -1,3 +1,4 @@
+// add score and end of quiz message to results page
 function updateScore(){
     if(parseInt(sessionStorage.getItem("percentageResult") >= 40)){
         document.getElementById("result").innerHTML = "Congratulations you got: "+ sessionStorage.getItem("percentageResult")+"%";
@@ -8,4 +9,3 @@ function updateScore(){
 }
 
 updateScore();
-
